2535370e1dba59297f7ee0cd1625273e2479bee7
Kaif Ahmed <hello@kaif.me>
2018-11-10 15:00:57 +0000

    Implemented doses#create and doses#new actions and view.

--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -5,6 +5,20 @@ class DosesController < ApplicationController
   end
 
   def create
-    raise
+    @dose = Dose.new(dose_params)
+    @dose.ingredient = Ingredient.find(params[:dose][:ingredient_id])
+    @dose.cocktail = Cocktail.find(params[:cocktail_id])
+
+    if @dose.save
+      redirect_to cocktail_path(@dose.cocktail)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def dose_params
+    params.require(:dose).permit(:description)
   end
 end
